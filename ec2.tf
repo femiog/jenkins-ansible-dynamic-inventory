cca011ee27e59e8896ab8daf3f727b4f7536e88f
femiog <111324048+femiog@users.noreply.github.com>
2022-12-08 05:04:25 -0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "app" {
     ami           = "ami-08e637cea2f053dfa"
     instance_type = "t2.micro"
-    security_groups = ["sg-0d539cb40a50a3662 - launch-wizard-2"]
+    security_groups_id = ["sg-091d2ac57442c814f"]
     key_name      =  "femiogkey2"
     tags = {
         Name = " tomcat"
